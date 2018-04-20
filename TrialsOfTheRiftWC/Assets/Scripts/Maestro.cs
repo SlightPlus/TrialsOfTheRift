2c8ed56cb4d9c56616d4c1434f1e0c18712d9f6a
no-na <noahknam@gmail.com>
2018-04-20 05:13:56 -0400

    -Volume will be persistent between scenes. (This change requires scene changes.)

--- TrialsOfTheRiftWC/Assets/Scripts/Maestro.cs
+++ TrialsOfTheRiftWC/Assets/Scripts/Maestro.cs
@@ -377,10 +377,10 @@ public sealed class Maestro : MonoBehaviour {
 			PlayAnnouncementTutorial();
 		/* if(SceneManager.GetActiveScene().name == "BuildSetUp")
 			PlayAnnouncementIntro(); */
-        am_masterMix.SetFloat("VolumeMaster",Constants.VolOptions.C_MasterVolume);
-        am_masterMix.SetFloat("VolumeVOI",Constants.VolOptions.C_VOIVolume);
-        am_masterMix.SetFloat("VolumeBGM",Constants.VolOptions.C_BGMVolume);
-        am_masterMix.SetFloat("VolumeSFX",Constants.VolOptions.C_SFXVolume);
+        // am_masterMix.SetFloat("VolumeMaster",Constants.VolOptions.C_MasterVolume);
+        // am_masterMix.SetFloat("VolumeVOI",Constants.VolOptions.C_VOIVolume);
+        // am_masterMix.SetFloat("VolumeBGM",Constants.VolOptions.C_BGMVolume);
+        // am_masterMix.SetFloat("VolumeSFX",Constants.VolOptions.C_SFXVolume);
 	}
 	
 	// Takes volatility level as a parameter.
