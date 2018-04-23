1e74c870b76d38c5b5c54f8f3392628a8a6dfb60
SlightPlus <nero.DDX@gmail.com>
2018-04-23 15:00:27 -0400

    > Added New Pause Banner to WarmUp.unity. > Resolved issue of extra selection arrow appearing when player backs out of options in Pause.

--- TrialsOfTheRiftWC/Assets/Scripts/PauseController.cs
+++ TrialsOfTheRiftWC/Assets/Scripts/PauseController.cs
@@ -25,6 +25,7 @@ public class PauseController : MonoBehaviour {
     [SerializeField]Text txt_blueScoreLabel;
     [SerializeField]Button butt_select;
     [SerializeField]Button butt_optSelect;
+    [SerializeField]Button butt_options;
     //private Player p_player;
     private float f_unPause;
     [SerializeField] Rewired.Integration.UnityUI.RewiredStandaloneInputModule rsim;
@@ -45,7 +46,6 @@ public class PauseController : MonoBehaviour {
 
             //Properly highlight the button.
             butt_select.Select();
-            //butt_select.OnSelect(null);
 
             Time.timeScale = 0;
             
@@ -68,8 +68,7 @@ public class PauseController : MonoBehaviour {
     public void CloseOptions() {
         go_options.SetActive(false);
         go_pause.SetActive(true);
-        butt_select.Select();
-        butt_select.OnSelect(null);
+        butt_options.Select();
     }
 
     public void GameReset() {
